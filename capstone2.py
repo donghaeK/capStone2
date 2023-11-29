9781b85bb6e6d4429b2e6438710bf39a356591ec
김동해 <143689692+donghaeK@users.noreply.github.com>
2023-11-29 10:08:14 +0900

    Add files via upload

--- capstone2.py
+++ capstone2.py
@@ -27,8 +27,8 @@ class ForecastLSTM:
     def __init__(self, random_seed: int = 1234):
         self.random_seed = random_seed
 
-df = pd.read_csv("/capStone2.csv")
-
+df = pd.read_csv("https://raw.githubusercontent.com/donghaeK/capStone2/main/capStone2.csv")
+df['Date'] = pd.to_datetime(df['Date'])
 def reshape_dataset(self, df: pd.DataFrame) -> np.array:
     # y 컬럼을 데이터프레임의 맨 마지막 위치로 이동
     if "y" in df.columns:
@@ -205,6 +205,130 @@ def build_and_compile_lstm_model(
 
 ForecastLSTM.build_and_compile_lstm_model = build_and_compile_lstm_model
 
+def fit_lstm(
+    self,
+    df: pd.DataFrame,
+    steps: int,
+    lstm_units: list,
+    activation: str,
+    dropout: float = 0,
+    seq_len: int = 16,
+    single_output: bool = False,
+    epochs: int = 200,
+    batch_size: int = None,
+    steps_per_epoch: int = None,
+    learning_rate: float = 0.001,
+    patience: int = 10,
+    validation_split: float = 0.3,
+    last_lstm_return_sequences: bool = False,
+    dense_units: list = None,
+    metrics: str = "mse",
+    check_point_path: str = None,
+    verbose: bool = False,
+    plot: bool = True,
+):
+    """
+    LSTM 기반 모델 훈련을 진행한다.
+
+    :param df: DataFrame for model train.
+    :param steps: Length to predict.
+    :param lstm_units: LSTM, Dense Layers
+    :param activation: Activation function for LSTM, Dense Layers.
+    :param dropout: Dropout ratio between Layers.
+    :param seq_len: Length of sequences. (Look back window size)
+    :param single_output: Select whether 'y' is a continuous value or a single value.
+    """
+
+    np.random.seed(self.random_seed)
+    tf.random.set_seed(self.random_seed)
+
+    # 훈련, 검증 데이터셋 생성
+    (
+        self.X_train,
+        self.y_train,
+        self.X_val,
+        self.y_val,
+    ) = self.split_train_valid_dataset(
+        df=df,
+        seq_len=seq_len,
+        steps=steps,
+        validation_split=validation_split,
+        single_output=single_output,
+        verbose=verbose,
+    )
+
+    # LSTM 모델 생성
+    n_features = df.shape[1] - 1
+    self.model = self.build_and_compile_lstm_model(
+        seq_len=seq_len,
+        n_features=n_features,
+        lstm_units=lstm_units,
+        activation=activation,
+        learning_rate=learning_rate,
+        dropout=dropout,
+        steps=steps,
+        last_lstm_return_sequences=last_lstm_return_sequences,
+        dense_units=dense_units,
+        metrics=metrics,
+        single_output=single_output,
+    )
+
+    # 모델 적합 과정에서 best model 저장
+    if check_point_path is not None:
+        # create checkpoint
+        checkpoint_path = f"checkpoint/lstm_{check_point_path}.h5"
+        checkpoint = ModelCheckpoint(
+            filepath=checkpoint_path,
+            save_weights_only=False,
+            save_best_only=True,
+            monitor="val_loss",
+            verbose=verbose,
+        )
+        rlr = ReduceLROnPlateau(
+            monitor="val_loss", factor=0.5, patience=patience, verbose=verbose
+        )
+        callbacks = [checkpoint, EarlyStopping(patience=patience), rlr]
+    else:
+        rlr = ReduceLROnPlateau(
+            monitor="val_loss", factor=0.5, patience=patience, verbose=verbose
+        )
+        callbacks = [EarlyStopping(patience=patience), rlr]
+
+    # 모델 훈련
+    self.history = self.model.fit(
+        self.X_train,
+        self.y_train,
+        batch_size=batch_size,
+        steps_per_epoch=steps_per_epoch,
+        validation_data=(self.X_val, self.y_val),
+        epochs=epochs,
+        use_multiprocessing=True,
+        workers=8,
+        verbose=verbose,
+        callbacks=callbacks,
+        shuffle=False,
+    )
+
+    # 훈련 종료 후 best model 로드
+    if check_point_path is not None:
+        self.model.load_weights(f"checkpoint/lstm_{check_point_path}.h5")
+
+    # 모델링 과정 시각화
+    if plot:
+        plt.figure(figsize=(12, 6))
+        plt.plot(self.history.history[f"{metrics}"])
+        plt.plot(self.history.history[f"val_{metrics}"])
+        plt.title("Performance Metric")
+        plt.xlabel("Epoch")
+        plt.ylabel(f"{metrics}")
+        if metrics == "mape":
+            plt.axhline(y=10, xmin=0, xmax=1, color="grey", ls="--", alpha=0.5)
+        plt.legend(["Train", "Validation"], loc="upper right")
+        plt.show()
+
+
+ForecastLSTM.fit_lstm = fit_lstm
+
 def forecast_validation_dataset(self) -> pd.DataFrame:
     # 검증 데이터셋의 실제 값(y)과, 예측 값(yhat)을 저장할 리스트 생성
     y_pred_list, y_val_list = list(), list()
@@ -236,9 +360,9 @@ def calculate_metrics(df_fcst: pd.DataFrame) -> dict:
     }
 
 ## 1) Train, Test 데이터 분리
-cutoff = "20180101"
-df_train = df[df.index < cutoff]
-df_test = df[df.index >= cutoff]
+cutoff = "2018-01-01"
+df_train = df[df.Date < cutoff]
+df_test = df[df.Date >= cutoff]
 
 ## 2) Sequence Length, 예측 기간(Step), Single Output 여부 등 정의
 seq_len = 5  # 과거 5주의 데이터를 feature로 사용
@@ -260,6 +384,7 @@ lstm_params = {
 }
 
 ## 4) 모델 훈련
+
 fl = ForecastLSTM()
 fl.fit_lstm(
     df=df_train,
@@ -273,4 +398,6 @@ fl.fit_lstm(
 df_fcst_val = fl.forecast_validation_dataset()
 val_loss = calculate_metrics(df_fcst=df_fcst_val)[metrics]
 print(f"{metrics} of validation dataset: {val_loss.round(3)}")
+
+
 
